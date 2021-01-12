4840450565cf8db0e54cf6ea062c9b8b1126323f
mateusdeitos <mate.deitos@hotmail.com>
2021-01-12 12:25:38 -0300

    Ajuste log error conectar DB

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -4,6 +4,7 @@ import config from '@config/database';
 
 createConnection(config)
   .then(() => console.log('✅ - Conectado ao DB'))
-  .catch(error =>
-    console.log(`❌ - Ocorreu um erro ao conectar no DB: ${error.message}`),
-  );
+  .catch(error => {
+    console.log(`❌ - Ocorreu um erro ao conectar no DB: ${error.message}`);
+    console.log({ config });
+  });
