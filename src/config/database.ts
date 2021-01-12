dd12a96ce5f99998c0d9294c6404a0e2c4f71450
mateusdeitos <mate.deitos@hotmail.com>
2021-01-12 11:13:57 -0300

    Ajuste variaveis ambiente typeorm

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,15 @@
 import { ConnectionOptions } from 'typeorm';
 import 'dotenv/config';
 
+const entities =
+  process.env.NODE_ENV === 'dev'
+    ? [String(process.env.TYPEORM_ENTITIES_LOCAL)]
+    : [String(process.env.TYPEORM_ENTITIES_DEPLOY)];
+const migrations =
+  process.env.NODE_ENV === 'dev'
+    ? [String(process.env.TYPEORM_MIGRATIONS_LOCAL)]
+    : [String(process.env.TYPEORM_MIGRATIONS_DEPLOY)];
+
 const config: ConnectionOptions = {
   type: process.env.TYPEORM_CONNECTION as 'mariadb',
   host: process.env.TYPEORM_HOST,
@@ -10,8 +19,8 @@ const config: ConnectionOptions = {
   database: process.env.TYPEORM_DATABASE,
   connectTimeout: 60000,
   acquireTimeout: 60000,
-  entities: [String(process.env.TYPEORM_ENTITIES)],
-  migrations: [String(process.env.TYPEORM_MIGRATIONS)],
+  entities,
+  migrations,
   cli: {
     migrationsDir: String(process.env.TYPEORM_MIGRATIONS_DIR),
   },
